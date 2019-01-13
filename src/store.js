34130f6f4e0cb169756f1afa4022f2a0ebcd0b3b
Jed Diaz <jemjemzms@gmail.com>
2019-01-13 17:03:15 +1300

    Adding initial files
    Adding files with vuex

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title: 'Jed Links',
+    links: [
+      'http://google.com',
+      'http://ricdiaz.com',
+      'http://youtube.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  },
+  mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    }
+  },
+  actions: {
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    }
+  }
+})
